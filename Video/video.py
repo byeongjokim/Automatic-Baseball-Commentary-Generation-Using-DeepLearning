e2f15aaf49a98798f01af009d583866417d390fb
Byeongjo Kim <bjbj8083@gmail.com>
2019-08-19 11:53:43 +0900

    new video

--- Video/video.py
+++ Video/video.py
@@ -45,3 +45,45 @@ def play(resource):
         diff = time() - start
         while diff < fps:
             diff = time() - start
+
+def play_bbox(resource):
+    def text_2_img(text):
+        img = Image.new('RGB', (1080, 50), color=(180, 180, 180))
+        font = ImageFont.truetype("gulim.ttc", 20)
+        #font = ImageFont.truetype("gulim.ttc", 30)
+        d = ImageDraw.Draw(img)
+        d.text((10, 10), text, font=font, fill=(0, 0, 0))
+
+        return np.asarray(img)
+
+    video = cv2.VideoCapture(settings.VIDEO_FILE)
+    video.set(1, settings.START_FRAME)
+    fps = 1 / 29.97
+
+    success, frame = video.read()
+    h, w, c = frame.shape
+    frameno = settings.START_FRAME + 1
+    resource.set_frameno(frameno)
+
+    text = resource.get_annotation()
+    textimage = text_2_img(text)
+
+    while (cv2.waitKey(1) != ord('q')):
+        start = time()
+        frameno = frameno + 1
+
+        success, frame = video.read()
+
+        resource.set_frame(frame)
+        resource.set_frameno(frameno)
+
+        if (resource.is_new_annotation_video()):
+            text = resource.get_annotation()
+            textimage = text_2_img(text)
+
+        frame[h - 100 : h - 50, 100 : w - 100] = textimage
+        cv2.imshow("Automatic Sports Commentary", frame)
+
+        diff = time() - start
+        while diff < fps:
+            diff = time() - start
